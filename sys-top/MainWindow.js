664035778723b1ac9a4ad31ef4e77beacd710af4
Vijayasekhar Deepak <Vijayasekhar.Deepak@ducenIT.com>
2020-06-15 21:48:52 +0530

    sys-top

---
+++ sys-top/MainWindow.js
@@ -0,0 +1,28 @@
+const { BrowserWindow } = require("electron");
+
+class MainWindow extends BrowserWindow {
+  constructor(file, isDev) {
+    super({
+      title: "SysTop",
+      width: isDev ? 800 : 355,
+      height: 500,
+      icon: `${__dirname}/assets/icons/icon.png`,
+      resizable: isDev ? true : false,
+      show: false,
+      opacity: 0.9,
+      skipTaskbar: true,
+      webPreferences: {
+        nodeIntegration: true,
+        backgroundThrottling: false,
+      },
+    });
+
+    this.loadFile(file);
+
+    if (isDev) {
+      this.webContents.openDevTools();
+    }
+  }
+}
+
+module.exports = MainWindow;
---
+++ sys-top/main.js
@@ -0,0 +1,99 @@
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require("electron");
+const { join } = require("path");
+const Store = require("./Store");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
+
+// Set env
+process.env.NODE_ENV = "production";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+let tray;
+
+// Initi Store & defaults
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
+
+function createMainWindow() {
+  mainWindow = new MainWindow("./app/index.html", isDev);
+}
+
+app.on("ready", () => {
+  createMainWindow();
+
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
+  });
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("close", (e) => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+
+    return true;
+  });
+
+  const icon = join(__dirname, "assets", "icons", "tray_icon.png");
+  tray = new AppTray(icon, mainWindow);
+});
+
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle"),
+      },
+    ],
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+// Set settings
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
